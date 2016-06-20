705abf77de29564fc174d1fca88407887d0b1536
Shokry <shokry92@gmail.com>
2016-06-20 18:49:04 +0200

    Backspace removes characters

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 document.querySelector("body").addEventListener("keypress", function(e) {
     var keynum;
     if(e.which){
-      keynum = e.which;
+        keynum = e.which;
+        document.querySelector("#prompt").innerHTML = document.querySelector("#prompt").innerHTML + String.fromCharCode(keynum);
     }
-    
-    document.querySelector("#prompt").innerHTML = document.querySelector("#prompt").innerHTML + String.fromCharCode(keynum)
+});
+
+document.querySelector("body").addEventListener("keydown", function(e) {
+    var keynum;
+    if(e.which){
+        if (e.which === 8) {
+            e.preventDefault();
+            document.querySelector("#prompt").innerHTML = document.querySelector("#prompt").innerHTML.slice(0, document.querySelector("#prompt").innerHTML.length - 1);
+        }
+    }
 });
