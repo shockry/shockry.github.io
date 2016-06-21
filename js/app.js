a3c2f27e8aa227f99d205ab8d3f567c38f0915ff
Shokry <shokry92@gmail.com>
2016-06-21 02:01:13 +0200

    Caret movement and character removal refactor

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
+//The character that the caret is currently at from the end of the input string
+var offsetFromRight = 0;
+//The location of the caret (above which character) in pixels
+var caretLocation = 0;
+
+function moveCaret (direction) {
+    //get document style sheet
+    var sheet = document.styleSheets[0];
+
+    //Remove first rule (always ".caret::after")
+    sheet.removeRule(0);
+
+    if (direction == 'left') {
+        //Every character is 8 pixels in width
+        caretLocation += 8;
+    } else {
+        //Every character is 8 pixels in width
+        caretLocation -= 8;
+    }
+
+    //move the caret to the left at every key stroke (the width of the character)
+    sheet.insertRule('.caret::after { right: '+ caretLocation +'px; }', 0);
+}
+
 document.querySelector("body").addEventListener("keypress", function(e) {
     var keynum;
     if(e.which){
@@ -9,9 +33,35 @@ document.querySelector("body").addEventListener("keypress", function(e) {
 document.querySelector("body").addEventListener("keydown", function(e) {
     var keynum;
     if(e.which){
-        if (e.which === 8) {
+        if (e.which === 8) { //Backspace
             e.preventDefault();
-            document.querySelector("#prompt").innerHTML = document.querySelector("#prompt").innerHTML.slice(0, document.querySelector("#prompt").innerHTML.length - 1);
+
+            //Constructing the input string, before caret + after caret
+            var rightHalf;
+            //If no arrow keys are pressed yet, left half is full string (minus last character), right half is empty
+            if (offsetFromRight === 0) {
+                rightHalf = '';
+            } else {
+                rightHalf = document.querySelector("#prompt").innerHTML.slice(offsetFromRight);
+            }
+
+            //offset from right is the character that the caret is at
+            //we want the string before it and after it, but not itself
+            var leftHalf = document.querySelector("#prompt").innerHTML.slice(0, offsetFromRight-1);
+
+            document.querySelector("#prompt").innerHTML = leftHalf + rightHalf;
+        }
+        else if (e.which === 37) { //Left arrow
+            if (document.querySelector("#prompt").innerHTML.length + offsetFromRight > 0){
+                offsetFromRight -= 1;
+                moveCaret('left');
+            }
+        }
+        else if (e.which === 39) { //Right arrow
+            if (offsetFromRight < 0){
+                offsetFromRight += 1;
+                moveCaret('right');
+            }
         }
     }
 });
